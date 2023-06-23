5561b2bf536dffdae6d9264ff0becbb1f1e31e5f
PieroGiovanni <pierogiovanni933@gmail.com>
2023-06-23 12:23:59 -0500

    added product type

--- backend/src/resolvers/category.ts
+++ backend/src/resolvers/category.ts
@@ -1,16 +1,16 @@
 import { Arg, Mutation, Resolver, Query } from "type-graphql";
-import { Category } from "@generated/type-graphql";
+import { ProductCategory } from "@generated/type-graphql";
 import { prisma } from "..";
 
 @Resolver()
 export class CategoryResolver {
-  @Mutation(() => Category)
-  async createCategory(@Arg("name") name: string): Promise<Category> {
-    return await prisma.category.create({ data: { name } });
+  @Mutation(() => ProductCategory)
+  async createCategory(@Arg("name") name: string): Promise<ProductCategory> {
+    return await prisma.productCategory.create({ data: { name } });
   }
 
-  @Query(() => [Category])
+  @Query(() => [ProductCategory])
   async getCategories() {
-    return await prisma.category.findMany();
+    return await prisma.productCategory.findMany();
   }
 }
