4608888bae94367746dfaff137c5fa2f086728df
PieroGiovanni <pierogiovanni933@gmail.com>
2023-06-23 01:01:01 -0500

    landing page visually finished

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,12 @@
-import Image from "next/image";
-import { Navbar } from "./components/Navbar";
-import { VerProdcutsButton } from "./components/VerProdcutsButton";
+import { Navbar } from "../components/Navbar";
+import { VerProdcutsButton } from "../components/VerProdcutsButton";
 
 export default function Home() {
   return (
     <div className="flex flex-col bg-[url('/4.webp')] h-screen bg-cover max-h-screen">
       <Navbar />
       <div className="flex-grow grid grid-rows-6">
-        <div className="flex row-start-6 justify-center ">
+        <div className="flex row-start-6 justify-center">
           <VerProdcutsButton />
         </div>
       </div>
