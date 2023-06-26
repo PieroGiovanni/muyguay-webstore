6aff5d146374368f961779d5e9a1a6717c4f0fe1
PieroGiovanni <pierogiovanni933@gmail.com>
2023-06-26 00:11:42 -0500

    wip showcase

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
+    "./src/**/**/*.{js,ts,jsx,tsx}",
     // "/.src/**/**/*{js,ts,jsx,tsx}",
     // "./src/components/ui/button.tsx",
   ],
