36d03f78cd920a7ae92ff1966c437d66d229a588
PieroGiovanni <pierogiovanni933@gmail.com>
2023-11-17 03:41:10 -0500

    added metadata and fixed minor bug

--- frontend/src/app/(user)/tienda/page.tsx
+++ frontend/src/app/(user)/tienda/page.tsx
@@ -3,19 +3,25 @@ import { ProductList } from "../../../components/ProductList";
 import { SearchProduct } from "../../../components/SearchProduct";
 import { getProductCategories } from "../../api/queries";
 
-interface PageProps {}
+interface PageProps {
+  searchParams: {
+    query?: string;
+    categoryId?: string;
+    orderBy?: string;
+  };
+}
 
-const Page = async ({}: PageProps) => {
+const Page = async ({ searchParams }: PageProps) => {
   const categories = await getProductCategories();
 
   return (
     <div className="pt-20 pb-5">
       <div className="flex w-full gap-2 items-center px-4">
-        <SearchProduct />
+        <SearchProduct searchParams={searchParams} />
         <p>Filtrar:</p>
         <FilterButton categories={categories} />
       </div>
-      <ProductList />
+      <ProductList searchParams={searchParams} />
     </div>
   );
 };
